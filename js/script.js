2cd942abe09c11fb60ba13b87ea85370ba25d3fd
Marsya <marsya.yeece.j@gmail.com>
2026-04-08 00:44:52 +0700

    Create Hero Section - Try add Hero Image Animation

--- js/script.js
+++ js/script.js
@@ -16,3 +16,27 @@ function changingImage(){
 }
 
 // setInterval(changingImage, 5000);
+
+function fadeOutAnimation(){
+    document.getElementById('hero-image-container').style.animation="fadeOutAnimation 1s";
+}
+
+function fadeInAnimation(){
+    document.getElementById('hero-image-container').style.animation="fadeInAnimation 1s";
+}
+
+function changingImageAnimation(){
+    //fade in 1s
+    img.classList.remove('fade-out');
+    img.classList.add('fade-in');
+
+    //wait 3s display img
+
+    //wait 4 secs after fade in + display, then do fade out 1s, wait 1s then change img
+    setTimeout(() =>{
+        //fade out
+        
+        //wait 1s, then change image
+    }, 4000);
+
+}
